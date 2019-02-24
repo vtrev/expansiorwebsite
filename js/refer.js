b7bc98f2b55a91979f9e0a4d0d77b25c20acb0d6
vtrev <baloyi.trev@gmail.com>
2019-02-24 04:33:54 +0200

    adding a function to execute the read me button to show more text on refer nav link

--- js/refer.js
+++ js/refer.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var dots = document.querySelector("#dots");
+    var moreText = document.querySelector("#more");
+    var btnText = document.querySelector(".btn-white");
 
+    //define a function called readMore 
+    btnText.addEventListener('click', () => {
+        if (dots.style.display === "none") {
+            dots.style.display = "inline-block";
+            btnText.innerHTML = "Read more";
+            moreText.style.display = "none";
+        } else {
+            dots.style.display = "none";
+            btnText.innerHTML = "Read less";
+            moreText.style.display = "inline-block";
+        }
+    })
     // //parallax
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, {});
     //carousel
+
 })

0f0aadf013c3917bdc4aa97890b423fc9fa4e2b6
vtrev <baloyi.trev@gmail.com>
2019-05-11 20:36:57 +0200

    wrap

--- js/introjs.js
+++ js/introjs.js
@@ -3,5 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
     // //parallax
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, {});
-    //carousel
+
+    let moreMembers = document.querySelector("#more-members");
+    let moreMembersButton = document.querySelector("#more-members-button");
+
+    moreMembersButton.addEventListener('click', () => {
+        console.log(moreMembersButton.innerHTML);
+
+        if (moreMembersButton.innerHTML === "See more") {
+
+            moreMembers.style.display = "block";
+            moreMembersButton.innerHTML = "See less";
+        } else {
+            moreMembersButton.innerHTML = "See more";
+            moreMembers.style.display = "none";
+
+        }
+    })
 })
